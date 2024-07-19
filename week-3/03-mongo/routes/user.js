b02f955610092b0d8c2b2f1589a7c2d04997468b
Prashant Shukla <prashantshukla766@gmail.com>
2024-07-19 21:52:51 +0530

    ...

--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -1,13 +1,33 @@
 const { Router } = require("express");
 const router = Router();
+const {User}=  require('../db/index.js')
 const userMiddleware = require("../middleware/user");
 
 // User Routes
 router.post('/signup', (req, res) => {
+    const {username , password} = req.body
+    if(!username || !password) {
+        throw new Error("User doesnt exist")
+    }
+    const user= User.findOne({username})
+    if(user) throw new Error("the user already exists")
+const newUser  = User.create({
+    username,
+    password
+})
+return res.status(200).json({
+    message: "User created successfully",
+})
+
     // Implement user signup logic
 });
 
-router.get('/courses', (req, res) => {
+router.get('/courses', async (req, res) => {
+const response= await Course.find({})
+return res.status(200).json({
+    message: "Courses fetched successfully",
+    courses: courses
+    })
     // Implement listing all courses logic
 });
 
